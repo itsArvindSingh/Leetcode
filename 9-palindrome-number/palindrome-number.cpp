6cde685c54b72fca35e2d289cfdef2f81bb09d03
ARVIND SINGH <222337570+itsArvindSingh@users.noreply.github.com>
2026-08-06 22:23:18 +0530

    Time: 1 ms (57.77%) | Memory: 8.5 MB (92.83%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -3,7 +3,7 @@ public:
     bool isPalindrome(int x) {
         if ( x < 0 ) return false;
         int temp = x, rev = 0;
-        while(x > 0){
+        while(x != 0){
             int rem = x%10;
             if (rev > INT_MAX / 10 || rev < INT_MIN / 10 ) return false;
             rev = rev * 10 + rem;
